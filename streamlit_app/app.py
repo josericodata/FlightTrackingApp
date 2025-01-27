5584b3dad92ba8e4a050e522c9ae0239389d9369
josericodata <maninastre@gmail.com>
2025-01-27 15:19:48 +0000

    Adjusing refreshing rate

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import pandas as pd
 from streamlit_folium import st_folium
 from modules.styles import set_background_image, apply_global_styles, apply_map_styles
 from modules.utils import (
@@ -12,7 +11,7 @@ from modules.utils import (
 # Set the page configuration
 st.set_page_config(
     page_title="Flight Tracking App",  # Page title
-    page_icon="✈️"  # Favicon (emoji or path to an image file)
+    page_icon="✈️"  # Favicon
 )
 
 # Path to the background image
@@ -30,6 +29,7 @@ if "flights_data" not in st.session_state:
     st.session_state["flight_map"] = None  # Cache the generated map
     st.session_state["filtered_flights"] = None  # Cache filtered flights
     st.session_state["selected_airline"] = None  # Cache selected airline
+    st.session_state["cached_map"] = None  # Add cached_map for map caching
 
 # Load airport data from session state
 airports = st.session_state["airports_data"]
@@ -50,17 +50,20 @@ if selected_airline != "Select Airline":
     # Fetch the airline code
     airline_code = airline_dropdown_data.loc[airline_dropdown_data["Airline"] == selected_airline, "ShortName"].values[0]
 
-    # Check if a new airline is selected
+    # Check if a new airline is selected or if the filtered flights have changed
     if st.session_state["selected_airline"] != airline_code:
         # Update session state
         st.session_state["selected_airline"] = airline_code
         st.session_state["filtered_flights"] = process_flights(airline_code)
         st.session_state["flight_map"] = generate_flight_map(st.session_state["filtered_flights"], airports)
 
-    # Display the map
-    if st.session_state["flight_map"]:
+        # Cache the generated map
+        st.session_state["cached_map"] = st.session_state["flight_map"]
+
+    # Display the cached map
+    if st.session_state["cached_map"]:
         st.subheader(f"Flights for {selected_airline}")
-        st_folium(st.session_state["flight_map"], width=1000, height=800)
+        st_folium(st.session_state["cached_map"], width=1000, height=800)
 
     # Display the filtered flight data
 if st.session_state["filtered_flights"] is not None and not st.session_state["filtered_flights"].empty:
